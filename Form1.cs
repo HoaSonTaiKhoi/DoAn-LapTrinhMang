746108f0aa58c9949b104aad9ed0a269acec9a87
YeonBi01 <22520481@gm.uit.edu.vn>
2024-06-04 01:41:25 +0700

    cập nhật sau khi fix xong cho link chieuphim

--- Form1.cs
+++ Form1.cs
@@ -6,10 +6,13 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static DoAn.Form1;
 using static System.Net.WebRequestMethods;
 
 namespace DoAn
@@ -37,7 +40,7 @@ namespace DoAn
         {
             myClient = new WebClient();
             myClient.Encoding = System.Text.Encoding.UTF8;
-            Uri uri = new Uri("https://betacinemas.vn/phim.htm");
+            Uri uri = new Uri("https://chieuphimquocgia.com.vn/");
             Stream replied = myClient.OpenRead(uri);
 
 
@@ -49,20 +52,50 @@ namespace DoAn
             replied.Close();
             myClient.Dispose();
 
+            //beta
+            //
+            // //*[@id=\"tab-1\"]/div/div/div/div/div/div/img
+            // //*[@id=\"tab-1\"]/div/div/div/div[2]/div/h3/a
+
+            ////*[@id="tab-1"]/div/div[1]/div/div[2]/div[1]/h3/a
+            //
+            // film_List.ToList().ForEach(film =>
+            // {
+            //   films.Add(new Film { Link_Img = film.Attributes["src"].Value });
+            // });
+            //  var temp_list = film_List.ToList();
+            //for (int i = 0; i < temp_list.Count; i++)
+            //{
+            //    films[i].Link_Uri = "https://betacinemas.vn" + temp_list[i].Attributes["href"].Value;
+            //    films[i].Film_Name = temp_list[i].InnerText;
+            // }
+            //
+
+            //
+            //img
+            ///html/body/div[5]/div/div/div[1]/div[1]/div[2]/div[1]/div[4]/div[1]/img
+            ///html/body/div[1]/div[3]/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/div/div[1]/img
+            ///html/body/div[1]/div[3]/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/div/div[1]/img
+
+            ///html/body/div[1]/div[3]/div[2]/div/div/div/div/div[2]/div/div/div/div[2]/div[1]/div/div/div[2]/div[2]/div[1]/a/h2
 
             films = new List<Film>();
             if (document == null) return;
-            var film_List = document.DocumentNode.SelectNodes("//*[@id=\"tab-1\"]/div/div/div/div/div/div/img");
+            var film_List = document.DocumentNode.SelectNodes("/html/body/div[5]/div/div/div/div[1]/div[1]/div/div/div/div/img");
             film_List.ToList().ForEach(film =>
             {
-                films.Add(new Film { Link_Img = film.Attributes["src"].Value });
+                string input = film.Attributes["src"].Value;
+                string pattern = @"_next.*?\.jpg";
+                MatchCollection matches = Regex.Matches(input, pattern);
+                films.Add(new Film { Link_Img = "https://chieuphimquocgia.com.vn/"+matches[0].Value+"&w=384&q=75"});
             });
 
-            film_List = document.DocumentNode.SelectNodes("//*[@id=\"tab-1\"]/div/div/div/div[2]/div/h3/a");
+            film_List = document.DocumentNode.SelectNodes("/html/body/div[5]/div/div/div/div[1]/div[1]/div/div/div/div[2]");
             var temp_list = film_List.ToList();
             for (int i = 0; i < temp_list.Count; i++)
             {
-                films[i].Link_Uri = "https://betacinemas.vn" + temp_list[i].Attributes["href"].Value;
+                Console.WriteLine(films[i].Link_Img);
+                //films[i].Link_Uri = "https://betacinemas.vn" + temp_list[i].Attributes["href"].Value;
                 films[i].Film_Name = temp_list[i].InnerText;
             }
 
@@ -80,6 +113,7 @@ namespace DoAn
                 Pbx_MoviePoster.TabIndex = 0;
                 Pbx_MoviePoster.TabStop = false;
                 Pbx_MoviePoster.ImageLocation = films[i].Link_Img;
+                Pbx_MoviePoster.SizeMode = PictureBoxSizeMode.StretchImage;
                 // 
                 // Label_MovieName
                 //
@@ -97,7 +131,7 @@ namespace DoAn
                 Panel_Carousel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
                 Panel_Carousel.Controls.Add(Label_MovieName);
                 Panel_Carousel.Controls.Add(Pbx_MoviePoster);
-                Panel_Carousel.Location = new System.Drawing.Point(56 + 315 *(i%3), 10+478 *(i/3));
+                Panel_Carousel.Location = new System.Drawing.Point(200 + 55*(i%3-1) + 315 *(i%3), 40 + 20*(i/3-1)+478 *(i/3));
                 Panel_Carousel.Name = "Panel_Carousel";
                 Panel_Carousel.Size = new System.Drawing.Size(315, 478);
                 Panel_Carousel.TabIndex = 0;
